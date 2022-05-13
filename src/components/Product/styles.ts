21428bc08d51e5246d6627791b48f40ed4e4fff7
Jonatas Santos <jonatassantos96@hotmail.com>
2022-05-12 22:11:51 -0300

    💄 Refactor placeholder input color for iOS platform

--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,8 +1,8 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 type TitleProps = {
   done: boolean;
-}
+};
 
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.WHITE};
@@ -26,16 +26,16 @@ export const Info = styled.View`
 export const Title = styled.Text<TitleProps>`
   font-size: 18px;
   font-family: ${({ theme }) => theme.FONTS.MEDIUM};
-  text-decoration-line: ${({ done }) => done ? 'line-through' : 'none'};
+  text-decoration-line: ${({ done }) => (done ? "line-through" : "none")};
 `;
 
 export const Quantity = styled.Text`
   font-size: 14px;
   font-family: ${({ theme }) => theme.FONTS.REGULAR};
-  color: ${({ theme }) => theme.COLORS.GRAY800}; 
+  color: ${({ theme }) => theme.COLORS.GRAY800};
 `;
 
 export const Options = styled.View`
-    height: 100%;
-    justify-content: space-around; 
+  height: 100%;
+  justify-content: space-around;
 `;
