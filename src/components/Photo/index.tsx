fa14aee9f01b647c27f4f959d4f485e5bcc478db
Jonatas Santos <jonatassantos96@hotmail.com>
2022-05-28 16:52:45 -0300

    ♻️  Refactor photo component title

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -20,13 +20,7 @@ export function Photo({ uri, ...rest }: Props) {
           <Image source={{ uri }} />
         ) : (
           <EmptyPhotoContainer>
-              <EmptyPhotoText>
-                clique para adicionar
-                ou para trocar a foto
-              </EmptyPhotoText>
-            </EmptyPhotoContainer >
-          )
-        }
+            <EmptyPhotoText>Nenhuma foto selecionada</EmptyPhotoText>
           </EmptyPhotoContainer>
         )}
       </Container>
