7caf0a07c75255f4abc2bcac13e5aea16280eb87
Jonatas Santos <jonatassantos96@hotmail.com>
2022-05-28 18:11:26 -0300

    🎨  Format code

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import auth from "@react-native-firebase/auth";
 
-import { Container, Account, Title, Subtitle } from './styles';
-import { ButtonText } from '../../components/ButtonText';
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
+import { ButtonText } from "../../components/ButtonText";
+import { Button } from "../../components/Button";
+import Input from "../../components/Input";
+
+import { Container, Account, Title, Subtitle } from "./styles";
 
 export function SignIn() {
   return (
@@ -11,21 +13,15 @@ export function SignIn() {
       <Title>MyShopping</Title>
       <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
 
-      <Input
-        placeholder="e-mail"
-        keyboardType="email-address"
-      />
+      <Input placeholder="e-mail" keyboardType="email-address" />
 
-      <Input
-        placeholder="senha"
-        secureTextEntry
-      />
+      <Input placeholder="senha" secureTextEntry />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={() => {}} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={() => {}} />
+        <ButtonText title="Criar conta" onPress={() => {}} />
       </Account>
     </Container>
   );
