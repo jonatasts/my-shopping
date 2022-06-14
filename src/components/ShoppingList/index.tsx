e7882c4b2bc4a39118305c0a6474d1680c1011d3
Jonatas Santos <jonatassantos96@hotmail.com>
2022-06-13 21:37:28 -0300

    🐛  Fix bug when product list is empty

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { FlatList } from "react-native";
 import firestore from "@react-native-firebase/firestore";
 
@@ -8,16 +8,14 @@ import Loading from "../Loading";
 import { styles } from "./styles";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>([]);
+  const [products, setProducts] = useState<ProductProps[] | undefined>([]);
   const [refreshing, setRefreshing] = useState(true);
   const [initialized, setInitialized] = useState(false);
   const TIME_INITIALIZED = initialized ? 500 : 2000;
 
-  useEffect(() => {
-    setTimeout(() => {
-      setInitialized(true);
-    }, 2000);
-  }, []);
+  const textLoading = useMemo(() => {
+    return initialized ? "Atualizando..." : "Carregando...";
+  }, [initialized]);
 
   useEffect(() => {
     const subscribe = firestore()
@@ -25,6 +23,8 @@ export function ShoppingList() {
       .orderBy("description")
       .onSnapshot((snapshot) => {
         setRefreshing(true);
+
+        if (snapshot) {
         const data = snapshot.docs.map((doc) => {
           return {
             id: doc.id,
@@ -33,9 +33,14 @@ export function ShoppingList() {
         }) as ProductProps[];
 
         setProducts(data);
+        }
 
         setTimeout(() => {
           setRefreshing(false);
+
+          if (!initialized) {
+            setInitialized(true);
+          }
         }, TIME_INITIALIZED);
       });
 
@@ -43,7 +48,7 @@ export function ShoppingList() {
   }, [TIME_INITIALIZED]);
 
   return refreshing ? (
-    <Loading label={initialized ? "Atualizando..." : "Carregando..."} />
+    <Loading label={textLoading} />
   ) : (
     <FlatList
       data={products}
